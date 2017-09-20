269e6466a57ec369ab099cb5abcc17af5ee87679
GithinjiWanjohi <gyth.wanjohi@gmail.com>
2017-09-20 20:33:36 +0300

    Added on 17th September 2017 at 7.27PM

--- buses.php
+++ buses.php
@@ -1,31 +1,32 @@
 
-<link  href= "buses.css" type="text/css" rel= "stylesheet">
+<link  href= "customer.css" type="text/css" rel= "stylesheet">
 <table class= "table">
 <tr>
-<th> Registration Number <th>
-<th> driver </th>
-<th> seats <th>
-<th> Destination <th>
+<th> Bus ID </th>
+<th> Registration Number </th>
+<th> Driver Name </th>
+<th> Seats </th>
 </tr>
 
 <?php
 
 include ('connection.php'); //includes the connection class
 
-$result = mysqli_query($db, "SELECT * FROM buses");//select all data in the buses table
+$sql = "SELECT * FROM bus";
+$result = $conn->query($sql) or trigger_error($conn->error."[$sql]");
+
+//$result = mysqli_query($db, "SELECT * FROM buses");//select all data in the buses table
 
 while($row= mysqli_fetch_array ($result))
 {      
 
  echo "<tr>";
- echo "<td>".$row['Registration']. "</td>";
- echo "<td>".$row['driver']. "</td>";
- echo "<td>".$row['seats']. "</td>";
- echo "<td>".$row['Destination']. "</td>";
+ echo "<td>".$row['Bus_ID']. "</td>";
+ echo "<td>".$row['Registration_No']. "</td>";
+ echo "<td>".$row['DriverName']. "</td>";
+ echo "<td>".$row['No_of_seats']. "</td>";
  echo "</tr>";
 };
 
 echo "</table>";//display the tale
-
-mysqli_close($db);//close database
 
